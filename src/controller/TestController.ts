32043e7eaa60d6675ab9e91f496ae34102411d93
anel-vukovic <anel.vukovic@pearshadow.com>
2023-11-04 16:41:55 +0100

    Implement post route /car-part #3

--- src/controller/TestController.ts
+++ src/controller/TestController.ts
@@ -1,17 +1,24 @@
+import {CarPart} from "../entity/CarPart";
+import {AppDataSource} from "../data-source";
+
 const express = require('express');
 const router = express.Router();
-const connection = require('../db'); // Import your MySQL connection
 
-// Retrieve data from the database
-router.get('/data', (req, res) => {
-  connection.query('SELECT * FROM car_part', (err, results) => {
-    if (err) {
-      console.error('Error executing query:', err);
-      res.status(500).json({ error: 'Internal server error' });
-      return;
+router.post('/car-part', async (req, res) => {
+    try {
+        const carPartRepository = AppDataSource.getRepository(CarPart);
+
+        // Create a new CarPart object from the request body
+        const newCarPart = carPartRepository.create(req.body);
+
+        // Save the new CarPart to the database
+        await carPartRepository.save(newCarPart);
+
+        res.status(201).json(newCarPart);
+    } catch (err) {
+        console.error('Error creating data:', err);
+        res.status(500).json({error: 'Internal server error'});
     }
-    res.status(200).json(results);
-  });
 });
 
 module.exports = router;
